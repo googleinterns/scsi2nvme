9ad044a35bee7474b29d364bf6bffd242c2f3d43
Emily Huang <eyhhuang@google.com>
2020-06-02 13:17:20 +0000

    WIP

--- lib/nvme_defs.h
+++ lib/nvme_defs.h
@@ -204,6 +204,370 @@ enum class NvmCommandOpCode : uint8_t {
   kReservationRelease = 0x15,
   // Vendor Specific = 0x80 to 0xff
 };
+enum class StatusCodeType : uint8_t {
+  kSctGeneric = 0x0,
+  kSctCommandSpecific = 0x1,
+  kSctMediaError 0x2,
+  kSctPath = 0x3,
+  /* 0x4-0x6 - reserved */
+  kSctVendorSpecific = 0x7,
+};
 
-} // namespace nvme_defs
-#endif
+/**
+*  * Generic command status codes
+*   */
+enum class GenericCommandStatusCode : uint8_t {
+  kScSuccess = 0x00,
+  kScInvalidOpcode = 0x01,
+  kScInvalidField = 0x02,
+  kScCommandIdConflict = 0x03,
+  kScDataTransferError = 0x04,
+  kScAbortedPowerLoss = 0x05,
+  kScInternalDeviceError = 0x06,
+  kScAbortedByRequest = 0x07,
+  kScAbortedSqDeletion = 0x08,
+  kScAbortedFailedFused = 0x09,
+  kScAbortedMissingFused = 0x0a,
+  kScInvalidNamespaceOrFormat = 0x0b,
+  kScCommandSequenceError = 0x0c,
+  kScInvalidSglSegDescriptor = 0x0d,
+  kScInvalidNumSglDescirptors = 0x0e,
+  kScDataSglLengthInvalid = 0x0f,
+  kScMetadataSglLengthInvalid = 0x10,
+  kScSglDescriptorTypeInvalid = 0x11,
+  kScInvalidControllerMemBuf = 0x12,
+  kScInvalidPrpOffset = 0x13,
+  kScAtomicWriteUnitExceeded = 0x14,
+  kScOperationDenied = 0x15,
+  kScInvalidSglOffset = 0x16,
+  /* 0x17 - reserved */
+  kScHostidInconsistentFormat = 0x18,
+  kScKeepAliveExpired = 0x19,
+  kScKeepAliveInvalid = 0x1a,
+  kScAbortedPreempt = 0x1b,
+  kScSanitizeFailed = 0x1c,
+  kScSanitizeInProgress = 0x1d,
+  kScSglDataBlockGranularityInvalid = 0x1e,
+  kScCommandInvalidInCmb = 0x1f,
+  kScLbaOutOfRange = 0x80,
+  kScCapacityExceeded = 0x81,
+  kScNamespaceNotReady = 0x82,
+  kScReservationConflict = 0x83,
+  kScFormatInProgress = 0x84,
+};
+
+/**
+*  * Command specific status codes
+*   */
+enum class CommandSpecificStatusCode : uint8_t {
+  kScCompletionQueueInvalid = 0x00,
+  kScInvalidQueueIdentifier = 0x01,
+  kScInvalidQueueSize = 0x02,
+  kScAbortCommandLimitExceeded = 0x03,
+  /* 0x04 - reserved */
+  kScAsyncEventRequestLimitExceeded = 0x05,
+  kScInvalidFirmwareSlot = 0x06,
+  kScInvalidFirmwareImage = 0x07,
+  kScInvalidInterruptVector = 0x08,
+  kScInvalidLogPage = 0x09,
+  kScInvalidFormat = 0x0a,
+  kScFirmwareReqConventionalReset = 0x0b,
+  kScInvalidQueueDeletion = 0x0c,
+  kScFeatureIdNotSaveable = 0x0d,
+  kScFeatureNotChangeable = 0x0e,
+  kScFeatureNotNamespaceSpecific = 0x0f,
+  kScFirmwareReqNvmReset = 0x10,
+  kScFirmwareReqReset = 0x11,
+  kScFirmwareReqMaxTimeViolation = 0x12,
+  kScFirmwareActivationProhibited = 0x13,
+  kScOverlappingRange = 0x14,
+  kScNamespaceInsufficientCapacity = 0x15,
+  kScNamespaceIdUnavailable = 0x16,
+  /* 0x17 - reserved */
+  kScNamespaceAlreadyAttached = 0x18,
+  kScNamespaceIsPrivate = 0x19,
+  kScNamespaceNotAttached = 0x1a,
+  kScThinprovisioningNotSupported = 0x1b,
+  kScControllerListInvalid = 0x1c,
+  kScDeviceSelfTestInProgress 0x1d,
+  kScBootPartitionWriteProhibited = 0x1e,
+  kScInvalidCtrlrId = 0x1f,
+  kScInvalidSecondaryCtrlrState = 0x20,
+  kScInvalidNumCtrlrResources = 0x21,
+  kScInvalidResourceId = 0x22,
+  kScConflictingAttributes = 0x80,
+  kScInvalidProtectionInfo = 0x81,
+  kScAttemptedWriteToRoRange = 0x82,
+};
+
+/**
+*  * Media error status codes
+*   */
+enum class MediaErrorStatusCode : uint8_t {
+  kWriteFaults = 0x80,
+  kUnrecoveredReadError = 0x81,
+  kGuardCheckError = 0x82,
+  kApplicationTagCheckError = 0x83,
+  kReferenceTagCheckError = 0x84,
+  kCompareFailure = 0x85,
+  kAccessDenied = 0x86,
+  kDeallocatedOrUnwrittenBlock = 0x87,
+};
+
+/**
+*  * Path related status codes
+*   */
+enum class PathStatusCode : uint8_t {
+  kInternalPathError = 0x00,
+  kControllerPathError = 0x60,
+  kHostPathError = 0x70,
+  kAbortedByHost = 0x71,
+};
+
+
+/**
+*  * Admin opcodes
+*   */
+enum class AdminOpcode : uint8_t {
+  kDeleteIoSq = 0x00,
+  kCreateIoSq = 0x01,
+  kGetLogPage = 0x02,
+  /* 0x03 - reserved */
+  kDeleteIoCq = 0x04,
+  kCreateIoCq = 0x05,
+  kIdentify = 0x06,
+  /* 0x07 - reserved */
+  kAbort = 0x08,
+  kSetFeatures = 0x09,
+  kGetFeatures = 0x0a,
+  /* 0x0b - reserved */
+  kAsyncEventRequest = 0x0c,
+  kNsManagement = 0x0d,
+  /* 0x0e-0x0f - reserved */
+  kFirmwareCommit = 0x10,
+  kFirmwareImageDownload = 0x11,
+  kDeviceSelfTest = 0x14,
+  kNsAttachment = 0x15,
+  kKeepAlive = 0x18,
+  kDirectiveSend = 0x19,
+  kDirectiveReceive = 0x1a,
+  kVirtualizationManagement = 0x1c,
+  kNvmeMiSend = 0x1d,
+  kNvmeMiReceive = 0x1e,
+  kDoorbellBufferConfig = 0x7c,
+  kFormatNvm = 0x80,
+  kSecuritySend = 0x81,
+  kSecurityReceive = 0x82,
+  kSanitize = 0x84,
+  kGetLbaStatus = 0x86,
+};
+
+/**
+*  * Nvm command set opcodes
+*   */
+enum class NvmOpcode : uint8_t {
+  kFlush = 0x00,
+  kWrite = 0x01,
+  kRead = 0x02,
+  /* 0x03 - reserved */
+  kWriteUncorrectable = 0x04,
+  kCompare = 0x05,
+  /* 0x06-0x07 - reserved */
+  kWriteZeroes = 0x08,
+  kDatasetManagement = 0x09,
+
+  kReservationRegister = 0x0d,
+  kReservationReport = 0x0e,
+
+  kReservationAcquire = 0x11,
+  kReservationRelease = 0x15,
+  kPdkNvmeSctGeneric = 0x0,
+  kSctCommandSpecific = 0x1,
+  kSctMediaError = 0x2,
+  kSctPath = 0x3,
+  /* 0x4-0x6 - reserved */
+  kSctVendorSpecific = 0x7,
+};
+
+/**
+*  * Generic command status codes
+*   */
+enum class GenericCommandStatusCode : uint8_t {
+  kSuccess = 0x00,
+  kInvalidOpcode = 0x01,
+  kInvalidField = 0x02,
+  kCommandIdConflict = 0x03,
+  kDataTransferError = 0x04,
+  kAbortedPowerLoss = 0x05,
+  kInternalDeviceError = 0x06,
+  kAbortedByRequest = 0x07,
+  kAbortedSqDeletion = 0x08,
+  kAbortedFailedFused = 0x09,
+  kAbortedMissingFused = 0x0a,
+  kInvalidNamespaceOrFormat = 0x0b,
+  kCommandSequenceError = 0x0c,
+  kInvalidSglSegDescriptor = 0x0d,
+  kInvalidNumSglDescirptors = 0x0e,
+  kDataSglLengthInvalid = 0x0f,
+  kMetadataSglLengthInvalid = 0x10,
+  kSglDescriptorTypeInvalid = 0x11,
+  kInvalidControllerMemBuf = 0x12,
+  kInvalidPrpOffset = 0x13,
+  kAtomicWriteUnitExceeded = 0x14,
+  kOperationDenied = 0x15,
+  kInvalidSglOffset = 0x16,
+  /* 0x17 - reserved */
+  kHostidInconsistentFormat = 0x18,
+  kKeepAliveExpired = 0x19,
+  kKeepAliveInvalid = 0x1a,
+  kAbortedPreempt = 0x1b,
+  kSanitizeFailed = 0x1c,
+  kSanitizeInProgress = 0x1d,
+  kSglDataBlockGranularityInvalid = 0x1e,
+  kCommandInvalidInCmb = 0x1f,
+
+  kLbaOutOfRange = 0x80,
+  kCapacityExceeded = 0x81,
+  kNamespaceNotReady = 0x82,
+  kReservationConflict = 0x83,
+  kFormatInProgress = 0x84,
+};
+
+/**
+*  * Command specific status codes
+*   */
+enum class CommandSpecificStatusCode : uint8_t {
+  kScCompletionQueueInvalid = 0x00,
+  kScInvalidQueueIdentifier = 0x01,
+  kScInvalidQueueSize = 0x02,
+  kScAbortCommandLimitExceeded = 0x03,
+  /* 0x04 - reserved */
+  kAsyncEventRequestLimitExceeded = 0x05,
+  kInvalidFirmwareSlot = 0x06,
+  kInvalidFirmwareImage = 0x07,
+  kInvalidInterruptVector = 0x08,
+  kInvalidLogPage = 0x09,
+  kInvalidFormat = 0x0a,
+  kFirmwareReqConventionalReset = 0x0b,
+  kInvalidQueueDeletion = 0x0c,
+  kFeatureIdNotSaveable = 0x0d,
+  kFeatureNotChangeable = 0x0e,
+  kFeatureNotNamespaceSpecific = 0x0f,
+  kFirmwareReqNvmReset = 0x10,
+  kFirmwareReqReset = 0x11,
+  kFirmwareReqMaxTimeViolation    = 0x12,
+  kFirmwareActivationProhibited     = 0x13,
+  kOverlappingRange                  = 0x14,
+  kNamespaceInsufficientCapacity    = 0x15,
+  kNamespaceIdUnavailable           = 0x16,
+                            /* 0x17 - reserved */
+  kNamespaceAlreadyAttached         = 0x18,
+  kNamespaceIsPrivate               = 0x19,
+  kNamespaceNotAttached             = 0x1a,
+  kThinprovisioningNotSupported     = 0x1b,
+  kControllerListInvalid            = 0x1c,
+  kDeviceSelfTestInProgress = 0x1d,
+  kBootPartitionWriteProhibited  = 0x1e,
+  kInvalidCtrlrId     = 0x1f,
+  kInvalidSecondaryCtrlrState  = 0x20,
+  kInvalidNumCtrlrResources  = 0x21,
+  kInvalidResourceId    = 0x22,
+  kConflictingAttributes   = 0x80,
+  kInvalidProtectionInfo    = 0x81,
+  kAttemptedWriteToRoRange  = 0x82,
+};
+
+/**
+*  * Media error status codes
+*   */
+enum class MediaErrorStatusCode : uint8_t {
+  kWriteFaults     = 0x80,
+  kUnrecoveredReadError   = 0x81,
+  kGuardCheckError      = 0x82,
+  kApplicationTagCheckError  = 0x83,
+  kReferenceTagCheckError    = 0x84,
+  kCompareFailure      = 0x85,
+  kAccessDenied      = 0x86,
+  kDeallocatedOrUnwrittenBlock     = 0x87,
+};
+
+/**
+*  * Path related status codes
+*   */
+enum class PathStatusCode : uint8_t {
+  kInternalPathError = 0x00,
+
+  kControllerPathError = 0x60,
+
+  kHostPathError = 0x70,
+  kAbortedByHost = 0x71,
+}
+//Persistent Reserve In Read Reservation Data No Reservation;
+
+#define MaxOpc 0xff
+
+/**
+*  * Admin opcodes
+*   */
+//Persistent Reserve In Read Reservation Data With Reservation;
+enum class AdminOpcode : uint8_t {
+  kDeleteIoSq = 0x00,
+  kCreateIoSq = 0x01,
+  kGetLogPage = 0x02,
+  /* 0x03 - reserved */
+  kDeleteIoCq = 0x04,
+  kCreateIoCq = 0x05,
+  kIdentify = 0x06,
+  /* 0x07 - reserved */
+  kAbort = 0x08,
+  kSetFeatures = 0x09,
+  kGetFeatures = 0x0a,
+  /* 0x0b - reserved */
+  kAsyncEventRequest = 0x0c,
+  kNsManagement = 0x0d,
+  /* 0x0e-0x0f - reserved */
+  kFirmwareCommit = 0x10,
+  kFirmwareImageDownload = 0x11
+  kDeviceSelfTest = 0x14,
+  kNsAttachment = 0x15,
+
+  kKeepAlive = 0x18,
+  kDirectiveSend =
+      //Persistent Reserve Out Parameter List0x19,
+  kDirectiveReceive = 0x1a,
+  kVirtualization Management = 0x1c,
+  kNvmeMiSend = 0x1d,
+  kNvmeMiReceive = 0x1e,
+
+  kDoorbellBufferConfig = 0x7c,
+
+  kFormatNvm = 0x80,
+  kSecuritySend = 0x81,
+  kSecurityReceive = 0x82,
+
+  kSanitize = 0x84,
+  kGetLbaStatus = 0x86,
+};
+
+/**
+*  * Nvm command set opcodes
+*   */
+enum class NvmOpcode : uint8_t {
+  kFlush = 0x00,
+  kWrite = 0x01,
+  kRead = 0x02,
+  /* 0x03 - reserved */
+  kWriteUncorrectable = 0x04,
+  kCompare = 0x05,
+  /* 0x06-0x07 - reserved */
+  kWriteZeroes = 0x08,
+  kDatasetManagement = 0x09,
+
+  kReservationRegister = 0x0d,
+  kReservationReport = 0x0e,
+
+  kReservationAcquire = 0x11,
+  kReservationRelease = 0x15,
+};
+
+} // namespace NvmeDefs
