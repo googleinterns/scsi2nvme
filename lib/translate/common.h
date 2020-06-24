3a63bcb5a58291d589e0f603b24ee819f35f2d5c
Justin Wei <wei.justin.a@gmail.com>
2020-06-24 17:25:34 -0500

    debug callback in common.h

--- lib/translate/common.h
+++ lib/translate/common.h
@@ -3,5 +3,12 @@
 
 namespace translator {
 const char NVME_VENDOR_IDENTIFICATION[9] = "NVMe    ";
+
+static void (*debug_callback)(const char*);
+
+static void setDebugCallback(void (*callback)(const char*)) {
+  debug_callback = callback;
 }
+
+}  // namespace translator
 #endif
