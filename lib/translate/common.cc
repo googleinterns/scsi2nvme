5809c9fa6b4d2ba50226be508f1ec1178a6799e1
Hunter Harloff <65253201+harloff-g@users.noreply.github.com>
2020-06-25 17:53:16 -0400

    Define common translation definitions (#77)
    * Add common definitions for translation lib
    
    * Use lambda & Variadic C
    
    * Style

---
+++ lib/translate/common.cc
@@ -0,0 +1,38 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.i// Copyright 2020 Google LLC
+
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "common.h"
+
+namespace translator {
+
+static void (*debug_callback)(const char*);
+
+void DebugLog(const char* format, ...) {
+  if (debug_callback == nullptr) return;
+  char buffer[1024];
+  va_list args;
+  va_start(args, format);
+  vsnprintf(buffer, 1024, format, args);
+  debug_callback(buffer);
+  va_end(args);
+}
+
+void SetDebugCallback(void (*callback)(const char*)) {
+  debug_callback = callback;
+}
+
+}  // namespace translator
