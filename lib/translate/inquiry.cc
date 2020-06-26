4929ccd441676c36ec618a3ae412a6af642f4f98
Justin Wei <wei.justin.a@gmail.com>
2020-06-25 19:45:39 -0500

    fixed comments on converting long to hex string

--- lib/translate/inquiry.cc
+++ lib/translate/inquiry.cc
@@ -173,7 +173,7 @@ scsi_defs::UnitSerialNumber TranslateUnitSerialNumberVpdResponse(
       // 6.1.3.1.1
       result.page_length = 40;
 
-      // convert 128-bit number into hex string
+      // convert 128-bit number into hex string, 64-bits at a time
       char hex_string[33];
       sprintf(hex_string, "%08lx", identify_namespace_data.nguid[0]);
       sprintf(&hex_string[16], "%08lx", identify_namespace_data.nguid[1]);
@@ -199,7 +199,7 @@ scsi_defs::UnitSerialNumber TranslateUnitSerialNumberVpdResponse(
       // 6.1.3.1.2
       result.page_length = 20;
 
-      // convert 128-bit number into hex string
+      // convert 64-bit number into hex string
       char hex_string[17];
       sprintf(hex_string, "%08lx", identify_namespace_data.eui64);
 
@@ -226,7 +226,7 @@ scsi_defs::UnitSerialNumber TranslateUnitSerialNumberVpdResponse(
       // 6.1.3.1.1
       result.page_length = 40;
 
-      // convert 128-bit number into hex string
+      // convert 128-bit number into hex string, 64-bits at a time
       char hex_string[33];
       sprintf(hex_string, "%08lx", identify_namespace_data.nguid[0]);
       sprintf(&hex_string[16], "%08lx", identify_namespace_data.nguid[1]);
