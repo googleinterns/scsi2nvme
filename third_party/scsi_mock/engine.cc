1721bce47d862ea774c1e01fd6c85e99226f63ed
Hunter Harloff <harloff@google.com>
2020-07-20 20:45:25 +0000

    Test with print

--- third_party/scsi_mock/engine.cc
+++ third_party/scsi_mock/engine.cc
@@ -12,5 +12,7 @@ void ScsiToNvme(unsigned char* cmd_buf, unsigned short cmd_len,
   unsigned long long lun, unsigned char* sense_buf, unsigned short sense_len,
   unsigned char* data_buf, unsigned short data_len, bool isDataIn) {
   translator::Translation t;
-  
+  translator::Span<nvme::GenericQueueEntryCpl> nvme_cpl;
+  translator::Span<uint8_t> buffer;
+  t.Complete(nvme_cpl, buffer);
 }
