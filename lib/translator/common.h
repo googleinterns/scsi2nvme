73f7dcb8e5678a2952cd039e2ca870a085078346
Hunter Harloff <65253201+harloff-g@users.noreply.github.com>
2020-07-24 14:40:59 -0400

    Add endianness check helper (#154)
    * Add endianness check helper
    
    * Style newline
    
    * Delete h q:qqq:Q1

--- lib/translator/common.h
+++ lib/translator/common.h
@@ -62,6 +62,10 @@ void SetDebugCallback(void (*callback)(const char*));
 void SetAllocPageCallbacks(uint64_t (*alloc_callback)(uint16_t),
                            void (*dealloc_callback)(uint64_t, uint16_t));
 
+// Returns true if system is little endian.
+// Returns false if system is big endian.
+bool IsLittleEndian();
+
 // Host to Network endianness transformation for uint64_t
 // Network endianness is always big endian
 // Converts value to big endian if Host is little endian
